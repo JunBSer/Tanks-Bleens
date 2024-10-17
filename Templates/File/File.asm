3b03dfa682142a96bd0385d305f105143bc2eab0
Roman <rgolovkl6@gmail.com>
2024-10-17 22:57:41 +0300

    feat: add read and parse obj file(need to add gen mesh)

--- Templates/File/File.asm
+++ Templates/File/File.asm
@@ -1,173 +1,285 @@
 proc ReadModel uses ebx,\
-     resultMesh, verticesPath, indicesPath
+        resultMesh, filePath
+        locals
+                hFile           dd      ?
+                fileData        dd      ?
+                fileSize        dd      ?
+                facesCount      dd      ?
+                verticesCount   dd      ?
+                vertices        dd      ?
+                indices         dd      ?
+        endl
 
-     locals
-        hFileVertices        dd      ?          ; Descriptor of file
-        hFileIndices         dd      ?          ; Descriptor of file
-        TrianglesCount       dd      ?          ; is already clear
-        IndicesCount         dd      ?
-        TriangleVertices     dd      ?          ; is already clear
-        TriangleIndex        dd      ?
-        resultVertices       dd      ?          ; is already clear
-        resultIndices        dd      ?
-        resultColors         dd      ?          ; is already clear
-     endl
+        xor        ebx, ebx
+        invoke     CreateFile, [filePath], GENERIC_READ, ebx, ebx, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, ebx
+        mov        [hFile], eax
+        invoke     GetFileSize, [hFile], ebx
+        mov        [fileSize], eax
+        invoke     HeapAlloc, [hHeap], HEAP_ZERO_MEMORY, eax
+        mov        [fileData], eax
+        invoke     ReadFile, [hFile], [fileData], [fileSize], ebx, ebx
+        invoke     CloseHandle, [hFile]
 
-     xor        ebx, ebx
-     invoke     CreateFile, [verticesPath], GENERIC_READ, ebx, ebx, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, ebx
-     mov        [hFileVertices], eax
+        stdcall    GetVerticesFacesCounts, [fileData], [fileSize]
+        mov        [facesCount], edx
+        mov        [verticesCount], eax
 
-     invoke     HeapAlloc, [hHeap], 8, sizeof.Vertex
-     mov        [TriangleVertices], eax
+        mov        eax, sizeof.Vertex
+        mov        ecx, [verticesCount]
+        mul        ecx
+        invoke     HeapAlloc, [hHeap], HEAP_ZERO_MEMORY, eax
+        mov        [vertices], eax
+        mov        [tempMesh + Mesh.vertices], eax
 
-     mov        esi, [resultMesh]
-     add        esi, Mesh.trianglesCount
+        mov        eax, 3 * 4
+        mov        ecx, [facesCount]
+        mul        ecx
+        invoke     HeapAlloc, [hHeap], HEAP_ZERO_MEMORY, eax
+        mov        [indices], eax
+        mov        [tempMesh + Mesh.indices], eax
 
-     invoke     ReadFile, [hFileVertices], esi, 4, ebx, ebx                     ; Read count of triangles
-     mov        edx, [esi]
-     mov        [TrianglesCount], edx
+        mov        edx, [facesCount]
+        mov        [tempMesh + Mesh.trianglesCount], edx
 
-     xor        edx, edx                                                ; Calc size of mesh triangles
-     mov        ecx, sizeof.Vertex
-     mov        eax, 3
-     mul        ecx
-     xor        edx, edx
-     mov        ecx, [TrianglesCount]
-     mul        ecx
-
-     mov        edi, [resultMesh]
-     push       eax                                                     ; Allocate memory in mesh
-     push       eax
-     invoke  HeapAlloc, [hHeap], 8   ; eax
-     mov     [resultVertices], eax
-     mov     [edi + Mesh.vertices], eax
-     invoke  HeapAlloc, [hHeap], 8   ; eax
-     mov     [resultColors], eax
-     mov     [edi + Mesh.colors], eax
-
-     mov        ecx, [TrianglesCount]
-.CopyCycle:
-     push       ecx
-     invoke     ReadFile, [hFileVertices], [TriangleVertices], 3 * sizeof.Vertex, ebx, ebx
-
-     mov        ecx, 9
-     mov        esi, [TriangleVertices]
-     mov        edi, [resultVertices]
-     rep movsd
-
-     mov        ecx, 9
-     mov        eax, 0.45
-     mov        edi, [resultColors]
-     rep stosd
-
-     add        [resultVertices], 3 * sizeof.Vertex
-     add        [resultColors], 3 * sizeof.Vertex
-     pop        ecx
-     loop       .CopyCycle
-     invoke     CloseHandle, [hFileVertices]
-
-     invoke     HeapFree, [hHeap], HEAP_NO_SERIALIZE, [TriangleVertices]
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-     invoke     CreateFile, [indicesPath], GENERIC_READ, ebx, ebx, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, ebx
-     mov        [hFileIndices], eax
-
-     invoke     HeapAlloc, [hHeap], 4
-     mov        [TriangleIndex], eax
-
-     invoke     ReadFile, [hFileIndices], [TriangleIndex], 4, ebx, ebx                     ; Read count of indices
-     mov        esi, [TriangleIndex]
-     mov        edx, [esi]
-     mov        [IndicesCount], edx
-
-     xor        edx, edx                                                ; Calc size of mesh triangles
-     mov        ecx, [IndicesCount]
-     mov        eax, 4
-     mul        ecx
-
-     mov        edi, [resultMesh]
-     invoke  HeapAlloc, [hHeap], 8, eax
-     mov     [resultIndices], eax
-     mov     [edi + Mesh.indices], eax
-
-     mov        ecx, [IndicesCount]
-.CopyCycleIndices:
-     push       ecx
-     invoke     ReadFile, [hFileIndices], [TriangleIndex], 4, ebx, ebx
-
-     mov        esi, [TriangleIndex]
-     mov        edi, [resultIndices]
-     movsd
-
-     add        [resultIndices], 4
-     pop        ecx
-     loop       .CopyCycleIndices
-     invoke     CloseHandle, [hFileIndices]
-
-     invoke     HeapFree, [hHeap], HEAP_NO_SERIALIZE, [TriangleIndex]
-
-     ret
+        stdcall    ParseData, [fileData], [fileSize], [vertices], [indices]
+        invoke     HeapFree, [hHeap], ebx, [fileData]
+        stdcall    GenerateMesh, tempMesh, [resultMesh], true
+        invoke     HeapFree, [hHeap], ebx, [vertices]
+        invoke     HeapFree, [hHeap], ebx, [indices]
+        ret
 endp
 
-proc StrToFloat:
-    fldz
-    xor eax, eax
-    xor edx, edx
-    mov ecx, 0
-    mov edi, 1
-    mov ebx, 0
+proc ParseData uses ebx edi esi,\
+        fileData, fileSize, vertices, indices
 
-    cmp byte [esi], '-'
-    jne .check_plus
-    mov ecx, 1
-    inc esi
+        mov     ecx, [fileSize]
+        mov     edi, [fileData]
+        mov     esi, [vertices]
+        mov     edx, [indices]
+        mov     eax, LF
+.parseLoop:
+        push     eax ecx edi
+        cmp     byte [edi], 'v'
+        jne     .Else
+        cmp     byte [edi + 1], ' '
+        je      .parseVertices
+.Else:
+        cmp     byte [edi], 'f'
+        jne     .Skip
+
+.parseIndices:
+        add     edi, 2
+        sub     ecx, 2
+        push    ecx edx
+        stdcall StrToInt, edi
+        pop     edx ecx
+        mov     [edx], eax
+        add     edx, 4
+        mov     eax, ' '
+        repne scasb
+
+        push    ecx edx
+        stdcall StrToInt, edi
+        pop     edx ecx
+        mov     [edx], eax
+        add     edx, 4
+        mov     eax, ' '
+        repne scasb
+
+        push    ecx edx
+        stdcall StrToInt, edi
+        pop     edx ecx
+        mov     [edx], eax
+        add     edx, 4
+        jmp     .Skip
+
+.parseVertices:
+        push    edx
+        add     edi, 2
+        sub     ecx, 2
+        push    ecx
+        stdcall StrToFloat, edi
+        pop     ecx
+        mov     [esi], eax
+        add     esi, 4
+        mov     eax, ' '
+        repne scasb
+
+        push    ecx
+        stdcall StrToFloat, edi
+        pop     ecx
+        mov     [esi], eax
+        add     esi, 4
+        mov     eax, ' '
+        repne scasb
+
+        push    ecx
+        stdcall StrToFloat, edi
+        pop     ecx
+        mov     [esi], eax
+        add     esi, 4
+        pop     edx
+.Skip:
+        pop     edi ecx eax
+        repne scasb
+        cmp     ecx, 0
+        jne    .parseLoop
+        ret
+endp
+
+proc GetVerticesFacesCounts uses edi esi,\
+        fileData, fileSize
+
+        xor     esi, esi        ; verticesCount
+        xor     edx, edx        ; facesCount
+        mov     ecx, [fileSize]
+        mov     edi, [fileData]
+        mov     eax, LF
+
+.CalcLoop:
+        cmp     byte [edi], 'v'
+        je      .IncVert
+        cmp     byte [edi], 'f'
+        jne     .SearchLF
+
+        inc     edx
+        jmp     .SearchLF
+.IncVert:
+        cmp     byte [edi + 1], ' '
+        jne     .SearchLF
+        inc     esi
+.SearchLF:
+        repne scasb
+        inc     ecx
+        loop    .CalcLoop
+
+        xchg    esi, eax
+        ret
+endp
+
+proc StrToFloat uses ebx edi esi,\
+        strFLOAT
+        locals
+                _10     dd      10.0
+                frac    dd      1.0
+                result  dd      0.0
+        endl
+        mov     esi, [strFLOAT]
+        fldz
+        xor     eax, eax
+        xor     edx, edx
+        mov     ecx, 0          ; minus flag
+        mov     edi, 10
+
+        cmp     byte [esi], '-'
+        jne     .check_plus
+        mov     ecx, 1
+        inc     esi
 
 .check_plus:
-    cmp byte [esi], '+'
-    jne .parse_number
-    inc esi
+        cmp     byte [esi], '+'
+        jne     .parse_number
+        inc     esi
 
 .parse_number:
 
 .next_digit:
-    cmp byte [esi], 0
-    je .end_conversion
+        cmp     byte [esi], 0
+        je      .end_conversion
 
-    cmp byte [esi], '.'
-    je .fraction_part
+        cmp     byte [esi], '.'
+        je      .fraction_part
 
-    sub byte [esi], '0'
-    cmp byte [esi], 9
-    ja .end_conversion
+        sub     byte [esi], '0'
+        cmp     byte [esi], 9
+        ja      .end_conversion
 
-    imul eax, edi
-    add eax, [esi]
-    inc esi
-    jmp .next_digit
+        mul     edi
+        movzx   ebx, byte [esi]
+        add     eax, ebx
+        inc     esi
+        jmp     .next_digit
 
 .fraction_part:
-    inc esi
-    mov edx, 1
-    mov ebx, 1
+        mov     [result], eax
+        fild    [result]
+        faddp
+        inc     esi
+        mov     edx, 1
+        mov     ebx, 1
 
 .parse_fraction:
-    cmp byte [esi], 0
-    je .end_conversion
+        cmp     byte [esi], 0
+        je      .end_conversion
 
-    sub byte [esi], '0'
-    cmp byte [esi], 9
-    ja .end_conversion
+        sub     byte [esi], '0'
+        cmp     byte [esi], 9
+        ja      .end_conversion
 
-    imul edx, edx, 10
-    push eax
-    mov eax, [esi]
-    cdq
-    idiv edx
-    pop eax
 
-    fld dword [esi]
-    fdiv dword [edx]
-    fadd dword [eax]
-    ret
+        movzx   eax, byte [esi]
+        push    eax
+        fild    dword [esp]
+        add     esp, 4
+        fld     [frac]
+        fdiv    [_10]
+        fmul    st1, st0
+        fstp    [frac]
+        faddp
+
+        inc     esi
+        jmp     .parse_fraction
+
+.end_conversion:
+        cmp     ecx, 1
+        jne     .skip
+        fchs
+.skip:
+        add     byte [esi], '0'
+        fstp    [result]
+        mov     eax, [result]
+        ret
+endp
+
+proc StrToInt uses ebx edi esi,\
+        strINT
+        mov     esi, [strINT]
+        xor     eax, eax
+        xor     edx, edx
+        mov     ecx, 0          ; minus flag
+        mov     edi, 10
+
+        cmp     byte [esi], '-'
+        jne     .check_plus
+        mov     ecx, 1
+        inc     esi
+
+.check_plus:
+        cmp     byte [esi], '+'
+        jne     .parse_number
+        inc     esi
+
+.parse_number:
+
+.next_digit:
+        cmp     byte [esi], 0
+        je      .end_conversion
+
+        sub     byte [esi], '0'
+        cmp     byte [esi], 9
+        ja      .end_conversion
+
+        mul     edi
+        movzx   ebx, byte [esi]
+        add     eax, ebx
+        inc     esi
+        jmp     .next_digit
+
+.end_conversion:
+        add     byte [esi], '0'
+        cmp     ecx, 1
+        jne     .Skip
+        neg     eax
+.Skip:
+        ret
 endp
